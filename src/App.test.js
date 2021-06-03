48db35de45866937e84642cddca68b57efcb87ce
Mateo Galic <mateogalic112@gmail.com>
2021-06-03 16:18:36 +0200

    First enzyme test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Enzyme, { shallow } from "enzyme";
+import EnzyimeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test('renders learn react link', () => {
+Enzyme.configure({ adapter: new EnzyimeAdapter() });
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders non-empty component without crashing", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toBe(true);
 });
