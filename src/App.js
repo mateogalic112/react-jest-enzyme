334f44e6465309316c2fe989e36498e19e06b2b9
Mateo Galic <mateogalic112@gmail.com>
2021-06-04 15:34:09 +0200

    Adding babel plugin.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div data-test="component-app"></div>;
+  return <div data-test="component-app">App</div>;
 }
 
 export default App;
